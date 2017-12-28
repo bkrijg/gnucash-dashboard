920a1b663e9d8ce3fb743b3b8db1796a9fed5c9b
bkrijg <bastiaan.krijgsman@gmail.com>
2017-12-28 14:12:46 +0100

    Code cleanup

--- column_chart.php
+++ column_chart.php
@@ -6,7 +6,7 @@ $month = date("m");
 $week = date("W");
 
 $qry = "select sum(if(post_year=$year,amount,0))amountcy,sum(if(post_year=$year-1,amount,0))amountpy,post_month from gnucash.vtransactions where category ='Boodschappen'
- and post_year in($year,$year-1) and post_month between 1 and $month-1 group by post_month order by post_month";
+    and post_year in($year,$year-1) and post_month between 1 and $month-1 group by post_month order by post_month";
 
 $result = mysqli_query($con,$qry);
 mysqli_close($con);
@@ -14,21 +14,19 @@ mysqli_close($con);
 $table = array();
 $table["cols"] = array(
 //Labels for the chart, these represent the column titles
-array("id" => "", "label" => "PostMonth", "type" => "number"),
-array("id" => "", "label" => "PriorYear", "type" => "number"),
-array("id" => "", "label" => "CurrentYear", "type" => "number")
-
+    array("id" => "", "label" => "PostMonth", "type" => "number"),
+    array("id" => "", "label" => "PriorYear", "type" => "number"),
+    array("id" => "", "label" => "CurrentYear", "type" => "number")
 );
 
 $rows = array();
 foreach($result as $row){
-$temp = array();
-
-//Values
-$temp[] = array("v" => (integer) $row["post_month"]);
-$temp[] = array("v" => (float) $row["amountpy"]);
-$temp[] = array("v" => (float) $row["amountcy"]);
-$rows[] = array("c" => $temp);
+    $temp = array();
+    //Values
+    $temp[] = array("v" => (integer) $row["post_month"]);
+    $temp[] = array("v" => (float) $row["amountpy"]);
+    $temp[] = array("v" => (float) $row["amountcy"]);
+    $rows[] = array("c" => $temp);
 }
 
 $result->free();
