36049f73720fb88d5551dfe7e5bc318dd2e93c78
bkrijg <bastiaan.krijgsman@gmail.com>
2017-12-28 14:14:17 +0100

    Code cleanup

--- db.php
+++ db.php
@@ -1,15 +1,9 @@
 <?php
 include('db_cr.php');
+
 $con = mysqli_connect($host,$user,$passwd);
-if(!$con)
-{
+if(!$con) {
   die("Could not connect: " . mysqli_error($con));
 }
-//$con = mysqli_select_db($con,"gnucash");
-//if(!$con)
-//{
-//	echo "Database Not Connected";
-//}
-
 ?>
 
